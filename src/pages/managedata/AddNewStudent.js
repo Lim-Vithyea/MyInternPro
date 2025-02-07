8dbc2429143d9090500eebbaa288129e6b71785d
Lim Vithyea <cheakh212@gmail.com>
2025-02-07 16:37:20 +0700

    new header for new students

--- src/pages/managedata/AddNewStudent.js
+++ src/pages/managedata/AddNewStudent.js
@@ -4,14 +4,11 @@ const AddNewStudent = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       {/* Header */}
-      <div className="w-full bg-[#1B1B69] flex items-center px-6 py-4 shadow-md">
-        <img src="/images/P10.png" alt="Emblem" className="w-12 h-12" />
-        <h1 className="text-white font-bold text-lg sm:text-xl ml-4">
-          Manage Data / Add Student    
-        </h1>
+      <div className="w-full bg-white flex items-center justify-center border-b-2">
+        <img src="/images/P10.png" alt="Emblem" className="w-10 h-10" />
+        <h1 className="text-start pt-3 pb-3 font-bold">Manage Data</h1>
       </div>
-
-      {/* Form Section */}
+      {/* Form */}
       <div className="w-[95%] mx-auto my-6 bg-white p-6 rounded-xl shadow-lg">
         <h1 className="font-bold text-2xl text-blue-700">Add Student</h1>
         <form className="mt-6">
@@ -34,7 +31,6 @@ const AddNewStudent = () => {
                 />
               </div>
             ))}
-            
             {/* Gender */}
             <div>
               <label className="block text-sm font-medium text-gray-700">Gender</label>
